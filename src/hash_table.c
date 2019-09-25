a44c7aeed58651c5860944629c82c26f09f39b28
Will Hoback <will.hoback@gmail.com>
2019-09-25 13:01:08 -0400

    insert

--- src/hash_table.c
+++ src/hash_table.c
@@ -82,3 +82,21 @@ static int ht_get_hash(const char *str, const int num_buckets, const int attempt
     //add 1 to hash_b to make sure its not 0
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
+
+void ht_insert(ht_hash_table *ht, const char *key, const char *value)
+{
+    ht_item *item = ht_new_item(key, value);
+    int index = ht_get_hash(item->key, ht->size, 0);
+    ht_item *cur_item = ht->items[index];
+    int i = 1;
+    //itr through until we find an empty bucket and insert
+    while(cur_item != NULL)
+    {
+        index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        ++i;
+    }
+    //insert our new item
+    ht->items[index] = item;
+    ht->count++;
+}
